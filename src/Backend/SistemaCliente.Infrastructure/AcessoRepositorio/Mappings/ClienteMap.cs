b69c660431ef737d939e74c19160f124165e74c5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 16:05:07 -0300

    Adiciona HasData para gerar migrações com dados

--- src/Backend/SistemaCliente.Infrastructure/AcessoRepositorio/Mappings/ClienteMap.cs
+++ src/Backend/SistemaCliente.Infrastructure/AcessoRepositorio/Mappings/ClienteMap.cs
@@ -1,19 +1,40 @@
 namespace SistemaCliente.Infrastructure.AcessoRepositorio.Mappings;
 
-    public class ClienteMap : IEntityTypeConfiguration<Cliente>
+public class ClienteMap : IEntityTypeConfiguration<Cliente>
+{
+    public void Configure(EntityTypeBuilder<Cliente> builder)
     {
-        public void Configure(EntityTypeBuilder<Cliente> builder)
-        {
-            builder.ToTable("Clientes");
+        builder.ToTable("Clientes");
 
-            builder.HasKey(c => c.Id);
+        builder.HasKey(c => c.Id);
 
-            builder.Property(c => c.NomeEmpresa)
-                .IsRequired()
-                .HasMaxLength(100);
+        builder.Property(c => c.NomeEmpresa)
+            .IsRequired()
+            .HasMaxLength(100);
 
-            builder.Property(e => e.Porte)
-                .IsRequired().
-                HasConversion<string>();
-        }
+        builder.Property(e => e.Porte)
+            .IsRequired().
+            HasConversion<string>();
+
+        builder.HasData(
+            new Cliente
+            {
+                Id = 1,
+                NomeEmpresa = "Empresa Pequena Ltda",
+                Porte = Domain.Enum.Porte.PEQUENA
+            },
+            new Cliente
+            {
+                Id = 2,
+                NomeEmpresa = "Empresa Media S.A.",
+                Porte = Domain.Enum.Porte.MEDIA
+            },
+            new Cliente
+            {
+                Id = 3,
+                NomeEmpresa = "Empresa Grande Corp.",
+                Porte = Domain.Enum.Porte.GRANDE
+            }
+        );
     }
+}
