8ed6d7df2fa6bb8855b3b6bdce80e6c05c4402c9
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 20:19:37 -0300

    Configura a rota e as sub rotas de Cliente

--- src/Frontend/SistemaCliente.UI.Web/src/app/app.routes.ts
+++ src/Frontend/SistemaCliente.UI.Web/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ClientesListComponent } from './clientes/clientes-list/clientes-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'clientes',
+    component: ClientesComponent,
+    children: [
+      {
+        path: '',
+        component: ClientesListComponent,
+      },
+    ],
+  },
+];
