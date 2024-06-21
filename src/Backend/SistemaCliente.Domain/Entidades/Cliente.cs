236ed2d27dcb009425613ec926d330927b10a90c
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 10:01:36 -0300

    Adiciona entidade Cliente

---
+++ src/Backend/SistemaCliente.Domain/Entidades/Cliente.cs
@@ -0,0 +1,8 @@
+namespace SistemaCliente.Domain.Entidades;
+
+    public class Cliente : EntidadeBase
+    {
+        public string NomeEmpresa { get; set; } = string.Empty;
+
+        public Porte Porte { get; set; }
+    }
