49cf8dcfcf852b1ec18238e2f7568c83d096e71c
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 10:53:39 -0300

    Adiciona global usings

---
+++ src/Backend/SistemaCliente.Application/Usings.cs
@@ -0,0 +1,13 @@
+global using System.Reflection;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using MediatR;
+global using FluentValidation;
+
+global using SistemaCliente.Application.Validation;
+
+
+
+
+
+
