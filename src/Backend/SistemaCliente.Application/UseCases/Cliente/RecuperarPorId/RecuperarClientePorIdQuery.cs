2e061be397628934e547dfd0906cdfd190068593
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 12:13:19 -0300

    Adiciona caso de uso RecuperarPorId de Cliente

---
+++ src/Backend/SistemaCliente.Application/UseCases/Cliente/RecuperarPorId/RecuperarClientePorIdQuery.cs
@@ -0,0 +1,27 @@
+namespace SistemaCliente.Application.UseCases.Cliente.RecuperarPorId;
+
+public record RecuperarClientePorIdQuery(long clienteId) : IRequest<RespostaClienteJson>;
+
+public class RecuperarClientePorIdQueryHandler : IRequestHandler<RecuperarClientePorIdQuery, RespostaClienteJson>
+{
+    private readonly IClienteReadOnlyRepositorio _repositorio;
+
+        public RecuperarClientePorIdQueryHandler(IClienteReadOnlyRepositorio repositorio) => _repositorio = repositorio;
+
+    public async Task<RespostaClienteJson> Handle(RecuperarClientePorIdQuery request, CancellationToken cancellationToken)
+    {
+        var cliente = await _repositorio.RecuperarPorId(request.clienteId);
+
+        if(cliente is null)
+            throw new Exception(ClienteMensagensDeErro.CLIENTE_NAO_ENCONTRADO);
+
+        return new RespostaClienteJson
+        (
+            cliente.Id,
+            cliente.NomeEmpresa,
+            (Communication.Enums.Porte)cliente.Porte,
+            cliente.DataCriacao
+        );
+    }
+}
+
