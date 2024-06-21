6b5b8d03247e58fc2d73c112598ceec70794ae21
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 12:15:18 -0300

    Adiciona global usings

--- src/Backend/SistemaCliente.Api/Usings.cs
+++ src/Backend/SistemaCliente.Api/Usings.cs
@@ -1,5 +1,4 @@
 global using Microsoft.AspNetCore.Mvc;
-global using Microsoft.AspNetCore.Diagnostics;
 
 global using MediatR;
 
@@ -8,7 +7,6 @@ global using SistemaCliente.Infrastructure;
 global using SistemaCliente.Application;
 global using SistemaCliente.Application.UseCases.Cliente.Registrar;
 global using SistemaCliente.Application.UseCases.Cliente.RecuperarTodos;
+global using SistemaCliente.Application.UseCases.Cliente.RecuperarPorId;
 
 global using SistemaCliente.Communication.Respostas.Cliente;
-
-global using SistemaCliente.Exceptions;
