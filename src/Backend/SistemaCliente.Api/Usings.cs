c640da8ea1d774808f6f763b76e761e9d495b58b
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 11:12:45 -0300

    Adiciona global usings

--- src/Backend/SistemaCliente.Api/Usings.cs
+++ src/Backend/SistemaCliente.Api/Usings.cs
@@ -1,2 +1,10 @@
-global using SistemaCliente.Application;
+global using Microsoft.AspNetCore.Mvc;
+
+global using MediatR;
+
 global using SistemaCliente.Infrastructure;
+global using SistemaCliente.Application;
+global using SistemaCliente.Application.UseCases.Cliente.Registrar;
+
+global using SistemaCliente.Communication.Respostas.Cliente;
+
