1bf3c6f40d92a9698ee0f4c6931d6801ae69c7a1
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 20:19:02 -0300

    Adiciona componente para listar todos clientes recuperados da api

---
+++ src/Frontend/SistemaCliente.UI.Web/src/app/clientes/clientes-list/clientes-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Cliente } from '../cliente.dto';
+import { Observable, lastValueFrom } from 'rxjs';
+import { ClienteService } from '../cliente.service';
+import { MaterialModule } from '../../material.module';
+import { LoadingBarComponent } from '../../loading-bar.component';
+import { AsyncPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { ClientesCardComponent } from './clientes-card/clientes-card.component';
+
+@Component({
+  selector: 'app-clientes-list',
+  standalone: true,
+  imports: [MaterialModule, LoadingBarComponent, AsyncPipe, RouterLink, ClientesCardComponent],
+  templateUrl: './clientes-list.component.html',
+  styles: ``,
+})
+export class ClientesListComponent {
+  clientes!: Cliente[];
+  clienteObservable!: Observable<Cliente[]>;
+
+  constructor(private clienteService: ClienteService) {}
+
+  async ngOnInit() {
+    this.clienteObservable = this.clienteService.RecuperarTodos();
+    this.clientes = await lastValueFrom(this.clienteObservable);
+  }
+}
