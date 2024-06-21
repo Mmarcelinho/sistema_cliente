e0700f935f3dee36a293a88ca5c4a713266b0673
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-21 10:23:08 -0300

    Adiciona global usings

---
+++ src/Backend/SistemaCliente.Infrastructure/Usings.cs
@@ -0,0 +1,9 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.Extensions.Configuration;
+global using Microsoft.Extensions.DependencyInjection;
+
+global using SistemaCliente.Domain.Entidades;
+
+global using SistemaCliente.Infrastructure.AcessoRepositorio;
+
