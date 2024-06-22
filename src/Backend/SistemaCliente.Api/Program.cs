d575b8a04fc3eff22df5a4ebda9532275b58ba08
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 12:56:00 -0300

    Configura HealthChecks e Swagger

--- src/Backend/SistemaCliente.Api/Program.cs
+++ src/Backend/SistemaCliente.Api/Program.cs
@@ -5,7 +5,10 @@ builder.Services.AddRouting(option => option.LowercaseUrls = true);
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(config =>
+{
+    config.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "SISTEMA DE CLIENTE API", Version = "1.0" });
+});
 
 builder.Services.AddCors(options =>
     {
@@ -24,6 +27,8 @@ builder.Services.AddProblemDetails();
 builder.Services.AdicionarInfrastructure(builder.Configuration);
 builder.Services.AdicionarApplication();
 
+builder.Services.AddHealthChecks().AddDbContextCheck<SistemaClienteContext>();
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -31,6 +36,16 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapHealthChecks("/Health", new HealthCheckOptions
+{
+    AllowCachingResponses = false,
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable,
+    }
+});
 
 app.UseExceptionHandler();
 
