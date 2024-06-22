c0e3bf51bde7eb92e82ca43d3f0b82a77b28094d
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 14:34:54 -0300

    Adiciona global usings

---
+++ tests/Utilitarios.Testes/Usings.cs
@@ -0,0 +1,6 @@
+global using Bogus;
+global using Moq;
+
+global using SistemaCliente.Domain.Entidades;
+global using SistemaCliente.Domain.Repositorios.Cliente;
+global using SistemaCliente.Communication.Requisicoes.Cliente;
