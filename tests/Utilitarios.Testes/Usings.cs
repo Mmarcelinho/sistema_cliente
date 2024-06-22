05081994ae18414121ca35f970e026d5680bab76
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 14:48:48 -0300

    Adiciona builder do command de Registrar e Atualizar Cliente

--- tests/Utilitarios.Testes/Usings.cs
+++ tests/Utilitarios.Testes/Usings.cs
@@ -3,4 +3,8 @@ global using Moq;
 
 global using SistemaCliente.Domain.Entidades;
 global using SistemaCliente.Domain.Repositorios.Cliente;
+
+global using SistemaCliente.Application.UseCases.Cliente.Commands.Registrar;
+global using SistemaCliente.Application.UseCases.Cliente.Commands.Atualizar;
+
 global using SistemaCliente.Communication.Requisicoes.Cliente;
