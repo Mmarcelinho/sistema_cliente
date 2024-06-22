37ca40f78e8f1d702a9e5e137e6641e9b756613f
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 14:34:38 -0300

    Adiciona builder do repositório

---
+++ tests/Utilitarios.Testes/Repositorios/ClienteReadOnlyRepositorioBuilder.cs
@@ -0,0 +1,22 @@
+namespace Utilitarios.Testes.Repositorios;
+
+public class ClienteReadOnlyRepositorioBuilder
+{
+    private readonly Mock<IClienteReadOnlyRepositorio> _repositorio;
+
+    public ClienteReadOnlyRepositorioBuilder() => _repositorio = new Mock<IClienteReadOnlyRepositorio>();
+
+    public ClienteReadOnlyRepositorioBuilder RecuperarTodos(List<Cliente> clientes)
+    {
+        _repositorio.Setup(repositorio => repositorio.RecuperarTodos()).ReturnsAsync(clientes);
+
+        return this;
+    }
+
+    public ClienteReadOnlyRepositorioBuilder RecuperarPorId(Cliente cliente)
+    {
+        _repositorio.Setup(repositorio => repositorio.RecuperarPorId(cliente.Id)).ReturnsAsync(cliente);
+
+        return this;
+    }
+}
