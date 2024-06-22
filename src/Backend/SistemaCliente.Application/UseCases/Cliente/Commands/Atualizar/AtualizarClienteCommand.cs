02da1f7e703448c87170efd65d9b00b426f4a682
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 16:00:42 -0300

    Adiciona validação para verificar se nome da empresa já existe

--- src/Backend/SistemaCliente.Application/UseCases/Cliente/Commands/Atualizar/AtualizarClienteCommand.cs
+++ src/Backend/SistemaCliente.Application/UseCases/Cliente/Commands/Atualizar/AtualizarClienteCommand.cs
@@ -1,24 +1,29 @@
 namespace SistemaCliente.Application.UseCases.Cliente.Commands.Atualizar;
 
-    public record AtualizarClienteCommand(long Id, RequisicaoClienteJson requisicaoCliente) : IRequest;
+public record AtualizarClienteCommand(long Id, RequisicaoClienteJson requisicaoCliente) : IRequest;
 
 public class AtualizarClienteCommandHandler : IRequestHandler<AtualizarClienteCommand>
 {
     private readonly IClienteUpdateOnlyRepositorio _repositorio;
 
+    private readonly IClienteWriteOnlyRepositorio _repositorioWrite;
+
     private readonly IUnidadeDeTrabalho _unidadeDeTrabalho;
 
-    public AtualizarClienteCommandHandler(IClienteUpdateOnlyRepositorio repositorio, IUnidadeDeTrabalho unidadeDeTrabalho)
+    public AtualizarClienteCommandHandler(IClienteUpdateOnlyRepositorio repositorio, IClienteWriteOnlyRepositorio repositorioWrite, IUnidadeDeTrabalho unidadeDeTrabalho)
     {
         _repositorio = repositorio;
+        _repositorioWrite = repositorioWrite;
         _unidadeDeTrabalho = unidadeDeTrabalho;
     }
 
     public async Task Handle(AtualizarClienteCommand request, CancellationToken cancellationToken)
     {
+        await Validar(request.requisicaoCliente);
+
         var cliente = await _repositorio.RecuperarPorId(request.Id);
 
-        if(cliente is null)
+        if (cliente is null)
             throw new Exception(ClienteMensagensDeErro.CLIENTE_NAO_ENCONTRADO);
 
         cliente.NomeEmpresa = request.requisicaoCliente.NomeEmpresa;
@@ -27,5 +32,13 @@ public class AtualizarClienteCommandHandler : IRequestHandler<AtualizarClienteCo
         _repositorio.Atualizar(cliente);
         await _unidadeDeTrabalho.Commit();
     }
+
+    private async Task Validar(RequisicaoClienteJson requisicao)
+    {
+        var clienteExiste = await _repositorioWrite.ExisteClienteComEmpresa(requisicao.NomeEmpresa);
+
+        if (clienteExiste)
+            throw new Exception(ClienteMensagensDeErro.CLIENTE_JA_REGISTRADO);
+    }
 }
- 
+
