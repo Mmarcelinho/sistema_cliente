d53f696cb5b8c012e22b1bbd67e66643f82124e5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 14:34:21 -0300

    Adiciona builder do dto de requisição do Cliente

---
+++ tests/Utilitarios.Testes/Requisicoes/RequisicaoClienteJsonBuilder.cs
@@ -0,0 +1,14 @@
+namespace Utilitarios.Testes.Requisicoes;
+
+    public class RequisicaoClienteJsonBuilder
+    {
+        public static RequisicaoClienteJson Build()
+        {
+            var faker = new Faker();
+
+            return new RequisicaoClienteJson(
+                faker.Company.CompanyName(),
+                (SistemaCliente.Communication.Enums.Porte)faker.Random.Int(0,2)
+            );
+        }
+    }
