d8b599e1d3e91d71b5929c222ea87b5b4e85db27
Mmarcelinho <marcelorosario2001@gmail.com>
2024-06-22 15:36:27 -0300

    Adiciona teste do validator Registrar de Cliente

---
+++ tests/Validators.Test/Cliente/Registrar/RegistrarClienteValidatorTest.cs
@@ -0,0 +1,48 @@
+namespace Validators.Test.Cliente.Registrar;
+
+public class RegistrarClienteValidatorTest
+{
+    [Fact]
+    public void Sucesso()
+    {
+        var validator = new RegistrarClienteValidator();
+
+        var requisicao = RegistrarClienteCommandBuilder.Build();
+
+        var resultado = validator.Validate(requisicao);
+
+        resultado.IsValid.Should().BeTrue();
+    }
+
+    [Fact]
+    public void NomeEmpresa_EmBranco_DeveRetornarErro()
+    {
+        var validator = new RegistrarClienteValidator();
+
+        var requisicao = RequisicaoClienteJsonBuilder.Build() with { NomeEmpresa = string.Empty };
+
+        var command = new RegistrarClienteCommand(requisicao);
+
+        var resultado = validator.Validate(command);
+
+        resultado.IsValid.Should().BeFalse();
+
+        resultado.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ClienteMensagensDeErro.EMPRESA_CLIENTE_NOME_EM_BRANCO));
+    }
+
+    [Fact]
+    public void Porte_Invalido_DeveRetornarErro()
+    {
+        var validator = new RegistrarClienteValidator();
+
+        var requisicao = RequisicaoClienteJsonBuilder.Build() with { Porte = (SistemaCliente.Communication.Enums.Porte)3 };
+
+        var command = new RegistrarClienteCommand(requisicao);
+
+        var resultado = validator.Validate(command);
+
+        resultado.IsValid.Should().BeFalse();
+
+        resultado.Errors.Should().ContainSingle().And.Contain(e => e.ErrorMessage.Equals(ClienteMensagensDeErro.EMPRESA_CLIENTE_PORTE_INVALIDO));
+    }
+}
---
+++ tests/Validators.Test/Usings.cs
@@ -0,0 +1,8 @@
+global using FluentAssertions;
+
+global using Utilitarios.Testes.Commands;
+global using Utilitarios.Testes.Requisicoes;
+
+global using SistemaCliente.Application.UseCases.Cliente.Commands.Registrar;
+
+global using SistemaCliente.Exceptions.MensagensDeErro;
