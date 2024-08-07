13f41c1ea0a718f9aaebdb46404641f9370c45b8
Mmarcelinho <marcelorosario2001@gmail.com>
2024-08-07 20:48:12 -0300

    Altera mensagens de erro para const

--- src/Backend/SistemaCliente.Exceptions/MensagensDeErro/ClienteMensagensDeErro.cs
+++ src/Backend/SistemaCliente.Exceptions/MensagensDeErro/ClienteMensagensDeErro.cs
@@ -2,11 +2,11 @@ namespace SistemaCliente.Exceptions.MensagensDeErro;
 
 public static class ClienteMensagensDeErro
 {
-    public static string EMPRESA_CLIENTE_NOME_EM_BRANCO = "O nome da empresa do cliente deve ser informado.";
+    public const string EMPRESA_CLIENTE_NOME_EM_BRANCO = "O nome da empresa do cliente deve ser informado.";
 
-    public static string EMPRESA_CLIENTE_PORTE_INVALIDO = "O porte da empresa do cliente é inválido.";
+    public const string EMPRESA_CLIENTE_PORTE_INVALIDO = "O porte da empresa do cliente é inválido.";
 
-    public static string CLIENTE_NAO_ENCONTRADO = "Cliente não encontrado.";
+    public const string CLIENTE_NAO_ENCONTRADO = "Cliente não encontrado.";
 
-    public static string CLIENTE_JA_REGISTRADO = "O cliente informado já está registrado na base de dados.";
+    public const string CLIENTE_JA_REGISTRADO = "O cliente informado já está registrado na base de dados.";
 }
