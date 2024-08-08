777fc784a20f2a0824abecdbf10d87ea6d4692d5
Mmarcelinho <marcelorosario2001@gmail.com>
2024-08-08 00:41:01 -0300

    Corrige casos de uso

--- src/Backend/SistemaCliente.Application/UseCases/Cliente/Commands/Atualizar/AtualizarClienteCommand.cs
+++ src/Backend/SistemaCliente.Application/UseCases/Cliente/Commands/Atualizar/AtualizarClienteCommand.cs
@@ -1,18 +1,13 @@
-
-
 namespace SistemaCliente.Application.UseCases.Cliente.Commands.Atualizar;
 
 public record AtualizarClienteCommand(long Id, RequisicaoClienteJson RequisicaoCliente) : IRequest;
 
 public class AtualizarClienteCommandHandler(
     IClienteUpdateOnlyRepositorio repositorioWrite,
-    IClienteReadOnlyRepositorio repositorioRead,
     IUnidadeDeTrabalho unidadeDeTrabalho) : IRequestHandler<AtualizarClienteCommand>
 {
     public async Task Handle(AtualizarClienteCommand request, CancellationToken cancellationToken)
     {
-        await Validar(request.RequisicaoCliente);
-
         var cliente = await repositorioWrite.RecuperarPorId(request.Id);
 
         if (cliente is null)
@@ -22,14 +17,6 @@ public class AtualizarClienteCommandHandler(
 
         repositorioWrite.Atualizar(cliente);
         await unidadeDeTrabalho.Commit();
-    }
-
-    private async Task Validar(RequisicaoClienteJson requisicao)
-    {
-        var clienteExiste = await repositorioRead.ExisteClienteComEmpresa(requisicao.NomeEmpresa);
-
-        if (clienteExiste)
-            throw new Exception(ClienteErrorsConstants.CLIENTE_JA_REGISTRADO);
     }
 }
 
--- src/Backend/SistemaCliente.Application/UseCases/Cliente/Commands/Registrar/RegistrarClienteCommand.cs
+++ src/Backend/SistemaCliente.Application/UseCases/Cliente/Commands/Registrar/RegistrarClienteCommand.cs
@@ -3,13 +3,11 @@ namespace SistemaCliente.Application.UseCases.Cliente.Commands.Registrar;
 public record RegistrarClienteCommand(RequisicaoClienteJson RequisicaoCliente) : IRequest<RespostaClienteJson>;
 
 public class RegistrarClienteCommandHandler(
-    IClienteWriteOnlyRepositorio repositorioWrite, IClienteReadOnlyRepositorio repositorioRead,
+    IClienteWriteOnlyRepositorio repositorioWrite,
     IUnidadeDeTrabalho unidadeDeTrabalho) : IRequestHandler<RegistrarClienteCommand, RespostaClienteJson>
 {
     public async Task<RespostaClienteJson> Handle(RegistrarClienteCommand request, CancellationToken cancellationToken)
     {
-        await Validar(request.RequisicaoCliente);
-
         var cliente = ClienteConversion.ToEntity(request.RequisicaoCliente);
 
         await repositorioWrite.Registrar(cliente);
@@ -19,13 +17,5 @@ public class RegistrarClienteCommandHandler(
         var (_cliente, _) = ClienteConversion.FromEntity(cliente, null!);
 
         return _cliente!;
-    }
-
-    private async Task Validar(RequisicaoClienteJson requisicao)
-    {
-        var clienteExiste = await repositorioRead.ExisteClienteComEmpresa(requisicao.NomeEmpresa);
-
-        if (clienteExiste)
-            throw new Exception(ClienteErrorsConstants.CLIENTE_JA_REGISTRADO);
     }
 }
